ba2c05bd1e70972bd05ed4204a524d8a5f638561
Joshh10 <jlawson41910@yahoo.com>
2025-02-23 21:20:28 -0500

    jQuery added

--- my-client-app/js/jQuery.js
+++ my-client-app/js/jQuery.js
@@ -1,8 +1,22 @@
-
 $(function () {
-    
-    $(".main-heading").hide().fadeIn(5000);
-    
-});
+    $(document).on("click", ".back-btn", function () {
+        $(".hero").fadeOut(100, function () {
+            $(this).css("background-image", "url('../images/lights.jpg')").fadeIn(1000);
+        });
+    });
 
+    $(document).on("click", ".form-group", function () {
+        alert('Form has been submitted!');
+    });
+
+    
   
+
+    $(".main-heading").hide().slideDown(2000);
+
+    $(".gallery-header").hide().fadeIn(2000);
+
+    $('.gallery-section').hide().fadeIn(2000);
+
+
+});
--- my-client-app/src/components/SectionOne.jsx
+++ my-client-app/src/components/SectionOne.jsx
@@ -12,12 +12,17 @@ function SectionOne() {
         <h1 className='main-heading'>Rebekah Photography</h1>
         <p>"Capturing moments, telling stories. I specialize in natural, timeless photography that brings your memories to life. Whether it’s portraits, events, or landscapes, I aim to create stunning images that you’ll cherish forever."</p>
         <div className='secone-btn'>
-            <Button to='/' className='btns' buttonStyle='outline' buttonSize='large'>
+            
+            <Button to="/" className='btns' buttonStyle='outline' buttonSize='large'>
                 Welcome
             </Button>
-            <Button className='btns' buttonStyle='solid' buttonSize='large'>
+            
+            <Button className='btns' buttonStyle='outline' buttonSize='large'>
               Gallery
             </Button>
+            <h2 className='back-btn'>
+              Try Me!
+            </h2>
         </div>
     </div>
   )
