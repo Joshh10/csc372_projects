5199c184dd3c97d14bcd2bcdce2a695d34efc1ca
Joshua Lawson <98487278+Joshh10@users.noreply.github.com>
2025-04-30 20:43:41 -0400

    Update SectionOne.jsx

--- my-client-app/src/components/SectionOne.jsx
+++ my-client-app/src/components/SectionOne.jsx
@@ -11,17 +11,7 @@ function SectionOne() {
   
         <h1 className='main-heading' id='main-h'>Rebekah Photography</h1>
         <p>"Capturing moments, telling stories. I specialize in natural, timeless photography that brings your memories to life. Whether it’s portraits, events, or landscapes, I aim to create stunning images that you’ll cherish forever."</p>
-        <div className='secone-btn'>
-            
-            <Button to="/" className='btns' buttonStyle='outline' buttonSize='large'>
-                Welcome
-            </Button>
-            
-            <Button className='btns' buttonStyle='outline' buttonSize='large'>
-              Gallery
-            </Button>
-            
-        </div>
+        
     </div>
   )
 }
